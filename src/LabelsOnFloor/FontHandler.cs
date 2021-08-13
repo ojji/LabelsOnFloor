449b1445fbfdbad1a906fd79c28df4c6c6bd7d97
ojji <zshun86@gmail.com>
2021-08-13 09:33:10 +0200

    Add Hungarian character set

--- src/LabelsOnFloor/FontHandler.cs
+++ src/LabelsOnFloor/FontHandler.cs
@@ -64,16 +64,63 @@ namespace LabelsOnFloor
 
         private int GetIndexInFontForChar(char c)
         {
-            var asciiVal = (int) c;
-            if (asciiVal < 33)
+            if (c < '!')
+            {
                 return 0;
-
-            if (asciiVal < 97)
-                return asciiVal - 32;
-            
-            if (asciiVal < 127)
-                return asciiVal - 58;
-
+            }
+            if (c < 'a')
+            {
+                return (int)(c - ' ');
+            }
+            if (c < '\u007f')
+            {
+                return (int)(c - ':');
+            }
+            // Á 00C1
+            if (c == '\u00C1')
+            {
+                return 69;
+            }
+            // É 00C9
+            if (c == '\u00C9')
+            {
+                return 70;
+            }
+            // Í 00CD
+            if (c == '\u00CD')
+            {
+                return 71;
+            }
+            // Ó 00D3
+            if (c == '\u00D3')
+            {
+                return 72;
+            }
+            // Ö 00D6
+            if (c == '\u00D6')
+            {
+                return 73;
+            }
+            // Ő 0150
+            if (c == '\u0150')
+            {
+                return 74;
+            }
+            // Ú 00DA
+            if (c == '\u00DA')
+            {
+                return 75;
+            }
+            // Ü 00DC
+            if (c == '\u00DC')
+            {
+                return 76;
+            }
+            // Ű 0170
+            if (c == '\u0170')
+            {
+                return 77;
+            }
             return 0;
         }
     }
--- src/LabelsOnFloor/Resources.cs
+++ src/LabelsOnFloor/Resources.cs
@@ -6,7 +6,7 @@ namespace LabelsOnFloor
     [StaticConstructorOnStartup]
     public class Resources
     {
-        public static Texture2D Font = ContentFinder<Texture2D>.Get("Consolas");
+        public static Texture2D Font = ContentFinder<Texture2D>.Get("Consolas_accented");
 
         public static Texture2D Rename = ContentFinder<Texture2D>.Get("Rename");
     }
